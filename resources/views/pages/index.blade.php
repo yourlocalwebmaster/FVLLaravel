8c5dae813a1b47855f4f20e37426030d5b26ece8
Grant Kimball <webmaster@yourlocalwebmaster.com>
2015-12-28 15:31:57 -0800

    responsive video on home

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,9 @@
 @extends('templates.page')
 @section('content')
     <div id="slider">
-        <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/3448831' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
+            <div class="videowrapper">
+                <iframe src='https://player.vimeo.com/video/3448831?color=ff9933&title=0&byline=0&portrait=0' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+        </div>
     </div>
 @stop
 @section('lowercontent')
